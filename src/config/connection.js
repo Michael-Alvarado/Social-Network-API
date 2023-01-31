c51d2d465b0e9b612f10b4d6fa96d4b8f0ef7bf7
mkdemon5 <alvaradomike5@gmail.com>
2023-01-30 18:15:57 -0600

    small tweaks made

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-	process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/socialnetDB';
+	process.env.MONGODB_URI || 'mongodb://localhost/social_network';
 
 connect(connectionString, {
 	useNewUrlParser: true,
